688532d13b36595ec4c7cbb27bcbc823d07587ef
Sergio Rubio <rubiojr@frameos.org>
2020-12-23 18:40:51 +0100

    Bump restic
    Upstream ref: 8efb874f4887721b2df98eae2f6a831e09c0cc6a

--- cmd/rapi/cmd_repository.go
+++ cmd/rapi/cmd_repository.go
@@ -3,9 +3,11 @@ package main
 import (
 	"context"
 	"fmt"
+	"sync/atomic"
 	"time"
 
 	"github.com/briandowns/spinner"
+	"github.com/rubiojr/rapi/repository"
 	"github.com/rubiojr/rapi/restic"
 	"github.com/urfave/cli/v2"
 )
@@ -50,6 +52,7 @@ func printInfo(c *cli.Context) error {
 	s.Start()
 
 	config := rapiRepo.Config()
+	packCount, countErr := countPacks(rapiRepo)
 	rapiRepo.LoadIndex(context.Background())
 	s.Stop()
 
@@ -57,8 +60,22 @@ func printInfo(c *cli.Context) error {
 	printRow("Location", rapiRepo.Backend().Location(), headerColor)
 	printRow("Chunker polynomial", config.ChunkerPolynomial.String(), headerColor)
 	printRow("Repository version", fmt.Sprintf("%d", config.Version), headerColor)
-	printRow("Packs", fmt.Sprintf("%d", len(rapiRepo.Index().Packs())), headerColor)
+	if countErr != nil {
+		printRow("Packs", "error", headerColor)
+	} else {
+		printRow("Packs", fmt.Sprintf("%d", packCount), headerColor)
+	}
 	printRow("Tree blobs", fmt.Sprintf("%d", rapiRepo.Index().Count(restic.TreeBlob)), headerColor)
 	printRow("Data blobs", fmt.Sprintf("%d", rapiRepo.Index().Count(restic.DataBlob)), headerColor)
 	return nil
+}
+
+func countPacks(repo *repository.Repository) (uint64, error) {
+	var packCount uint64
+	err := rapiRepo.List(context.Background(), restic.PackFile, func(id restic.ID, packSize int64) error {
+		atomic.AddUint64(&packCount, 1)
+		return nil
+	})
+
+	return packCount, err
 }
